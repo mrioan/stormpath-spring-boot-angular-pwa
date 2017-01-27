a6f52d901e7040610c1453ebb3291992b0d28250
Mario <mario@rtMBP.local>
2017-01-27 16:17:59 -0300

    initial commit

---
+++ server/src/main/java/com/example/BeerController.java
@@ -0,0 +1,35 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class BeerController {
+
+    private BeerRepository repository;
+
+    public BeerController(BeerRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/good-beers")
+    public Collection<Map<String, String>> goodBeers() {
+
+        return repository.findAll().stream()
+                .filter(this::isGreat)
+                .map(b -> {
+                    Map<String, String> m = new HashMap<String, String>();
+                    m.put("name", b.getName());
+                    return m;
+                }).collect(Collectors.toList());
+    }
+
+    private boolean isGreat(Beer beer) {
+        return !beer.getName().equals("Budweiser") &&
+                !beer.getName().equals("Coors Light") &&
+                !beer.getName().equals("PBR");
+    }
+}
